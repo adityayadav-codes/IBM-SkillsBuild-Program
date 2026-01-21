f1fb5702fd2716c3892d9fa1c3d73944ccccd5f7
adityayadav-codes <adityayadav2905@gmail.com>
2026-01-21 09:33:43 +0530

    print hello world

--- demo.py
+++ demo.py
@@ -1,3 +1,26 @@
-a = [10,20,30,40,50]
-a[3] = 100
-print(a)
+# file = open("example.txt", "w")
+# file.write("This is a file handling example.")
+# file.close()
+# file = open("example.txt", "r")
+# content = file.read()
+# print(content)
+# file.close()
+# name =input("Enter your name: ")
+# print(f"You entered: {name}")
+#input -> data given to the program by the user
+#output data -> data given by the program to the user
+from os import sep
+
+
+# a=int(input("Enter a number: "))
+# b=int(input("Enter another number: "))
+# print(f"The sum of {a} and {b} is {a+b}")
+# n = 9
+
+# b = str(a)
+# print()
+# print(type(b))
+print("hello", "world", sep=" ", end = "!")
+
+
+
